0afbfdf979acf195f00e676b6d94767e58622635
zakihussain <szakihussain123@gmail.com>
2021-06-22 04:53:08 -0700

    cc

--- PROJECT 1/script1.js
+++ PROJECT 1/script1.js
@@ -24,27 +24,33 @@ function isValidEmail(email) {
     re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
-
-//Function to check if required feilds are written
-function checkRequired(inputArray) {
-   inputArray.forEach(function(input) {
-       if ( input.value === '' ) {
-           console.log(input.id);
-           showError(input,`${getFeildId(input)} is required` );
-       } else {
-           showSuccess(input);
-       }
-   }); 
-
-   //Fucntion to get the ideal input field
-   function getFeildId(input) {
-      return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-   }
-
-
-// This is an event listener 
+// This is an event listenerconst 
 form.addEventListener('submit',function(e) {
     e.preventDefault();
 
-    checkRequired([username,email,password,password2]);
+    if ( username.value === '' ) {
+        showError(username,'Username is required')
+    } else {
+        showSuccess(username);
+    }
+
+    if ( email.value === '' ) {
+        showError(email,'Email is required')
+    } else if (!isValidEmail(email.value)) {
+        showError(email,'Email is invalid')
+    } else {
+        showSuccess(email);
+    }
+
+    if ( password.value === '' ) {
+        showError(password,'Password is required')
+    } else {
+        showSuccess(password);
+    }
+
+    if ( password2.value === '' ) {
+        showError(password2,'Confirm Password is required')
+    } else {
+        showSuccess(password2);
+    }
 })
